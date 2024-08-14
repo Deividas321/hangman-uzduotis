5676b63068cccfcff5145daa041f028e64971877
Deividas <kliucevicd@gmail.com>
2024-08-14 12:59:58 +0300

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: 'https://github.com/Deividas321/hang-man-bandymas', // Replace with your GitHub repository name
   plugins: [react()],
-})
+});
